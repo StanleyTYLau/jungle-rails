9e157c560a8fa73b76232d47c119f851bc38f417
Stanley Lau <stanley.tyl@gmail.com>
2019-01-18 17:09:22 +0000

    initial commit, mailer generated

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,22 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.preview.subject
+  #
+
+  default from: 'notifications@example.com'
+
+  def preview
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
